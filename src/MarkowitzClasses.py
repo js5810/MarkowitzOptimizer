069d5a367138ba26f85f33c14b4fd72ddd83bf83
js5810 <js5810@columbia.edu>
2024-07-20 15:15:41 -0400

    feat: covariance matrix of returns

--- src/MarkowitzClasses.py
+++ src/MarkowitzClasses.py
@@ -3,9 +3,10 @@ from datetime import datetime
 import json
 import re
 import pandas as pd
+import numpy as np
 import yfinance as yf
 from pyetfdb_scraper.etf import ETF, load_etfs
-from utils import extract_percent
+from utils import extract_percent, find_closest_date, annualized_return, count_days
 
 ETF_FILE = 'data/etf_returns.json'
 USER_AGENT_FILE = 'data/user-agents.txt'
@@ -15,7 +16,7 @@ RETURN_PERIODS = ['1_month_return',
                   '1_year_return',
                   '3_year_return',
                   '5_year_return']
-STOCK_HORIZON = '5y'
+STOCK_HORIZON = '5y'  # how long in the past to consider historical returns
 
 class MarkowitzOptimizer:
     def __init__(self, stocks: list[tuple[str, str]]):
@@ -42,7 +43,7 @@ class MarkowitzOptimizer:
         user_agent_list = self.get_user_agents()
         user_agent_count = len(user_agent_list)
         etf_dict = {}
-        for etf_symbol in full_etf_list[0:100]:
+        for etf_symbol in full_etf_list[0:200]:
             user_agent = user_agent_list[randrange(user_agent_count)]  # use a random user agent each time
             etf = ETF(etf_symbol, user_agent)
             curr_dict = {}
@@ -58,17 +59,23 @@ class MarkowitzOptimizer:
         return etf_dict
     
     def historical_returns(self, stock_name: str) -> list[int]:
-        """Produces a list containing the 6 (annualized) returns in percent"""
+        """Produces a list containing the 6 (annualized) stock returns in percent"""
         stock_obj = yf.Ticker(stock_name)
-        stock_df = stock_obj.history(period=STOCK_HORIZON)
-        print(stock_df['Close']['2019-07-22 00:00:00-04:00'])
+        closing_prices = stock_obj.history(period=STOCK_HORIZON)['Close']
+        recorded_dates = [closing_prices.index[i].date() for i in range(len(closing_prices))]
+        result = []
+        for horizon in RETURN_PERIODS:
+            closest_id = find_closest_date(recorded_dates, count_days(horizon))
+            last_price = float(closing_prices.iloc[closest_id]) # numpy float to float
+            curr_price = float(closing_prices.iloc[-1])
+            annualized_val = annualized_return(curr_price, last_price, horizon)
+            result.append(annualized_val)
+        return result
 
     def compute_portfolio_variance(self) -> int:
         return 0
-
-    def suggest_etfs(self):
-        if self.should_update_data():
-            self.update_etf_data()
+    
+    def build_returns_matrix(self):
         with open(ETF_FILE, encoding='utf-8', mode='r') as file:
             etf_dict = json.load(file)
         returns_matrix = []
@@ -84,3 +91,10 @@ class MarkowitzOptimizer:
             shares = stock[1]
             returns_list = self.historical_returns(stock_name)
             returns_matrix.append(returns_list)
+        return returns_matrix
+
+    def suggest_etfs(self):
+        if self.should_update_data():
+            self.update_etf_data()
+        returns_matrix = self.build_returns_matrix()
+        return np.cov(returns_matrix)
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,48 @@
+import numpy as np
+import datetime
+import math
 import re
 from typing import Optional
 
 def extract_percent(s: str) -> Optional[float]:
+    """Extract the number from a percentage string"""
     match = re.search(r'-?\d+(\.\d+)?', s)
     if match:
         return float(match.group())
     return None
+
+'''def extract_date(s: str) -> Optional[str]:
+    match = re.search(r'\b(\d{4})-(\d{2})-(\d{2})\b', s)
+    if match:
+        return match.group()
+    return None'''
+
+def find_closest_date(date_list: list[datetime.date], n: int) -> int:
+    """Return index of date that is closest to being n days before today"""
+    target_date = datetime.datetime.now().date() - datetime.timedelta(days = n)
+    try:
+        date_diff = np.abs([date - target_date for date in date_list])
+        return date_diff.argmin(0)
+    except ValueError:
+        print("There were no dates for this stock")
+
+def count_days(horizon: str):
+    if horizon == '1_month_return':
+        return 30
+    elif horizon == '3_month_return':
+        return 90
+    elif horizon == 'ytd_return':
+        now = datetime.datetime.now()
+        year_start = datetime.datetime(now.year, 1, 1)
+        return (now - year_start).days
+    elif horizon == '1_year_return':
+        return 365
+    elif horizon == '3_year_return':
+        return 1095
+    else:
+        return 1825
+
+def annualized_return(curr_price: float, last_price: float, horizon: str):
+    if horizon in {'1_month_return', '3_month_return', 'ytd_return'}:
+        return 100.0 * (curr_price-last_price)/last_price
+    return 100.0 * (math.pow((curr_price/last_price), 1/(count_days(horizon)/365)) - 1)  # annualize if >= 1 year
